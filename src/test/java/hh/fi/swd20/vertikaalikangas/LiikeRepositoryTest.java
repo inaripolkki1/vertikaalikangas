5b1e151628d44d9c4db612d566ce2a4106819dab
inaripolkki1 <bgm038@myy.haaga-helia.fi>
2021-04-22 03:43:34 +0300

    uus yritys

--- src/test/java/hh/fi/swd20/vertikaalikangas/LiikeRepositoryTest.java
+++ src/test/java/hh/fi/swd20/vertikaalikangas/LiikeRepositoryTest.java
@@ -5,11 +5,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
 import hh.fi.swd20.vertikaalikangas.domain.Liike;
 import hh.fi.swd20.vertikaalikangas.domain.LiikeRepository;
 
